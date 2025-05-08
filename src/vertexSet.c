4748e489c658073937625cec0d9cb7ce62dda18d
Frieder Wolpert-Veit <frieder@wolpert-veit.de>
2025-05-08 15:57:25 +0200

    Moved to master_project

--- src/vertexSet.c
+++ src/vertexSet.c
@@ -1,5 +1,6 @@
 
 #include "vertexSet.h"
+#include "bitmap_custom.h"
 #include <stddef.h>
 #include <stdint.h>
 #include <mpi.h>
@@ -10,28 +11,41 @@ void Vertexset_Init(Vertexset* vs, uint32_t maxsize){
     vs->maxsize = maxsize;
 
     // init bitarray
-    size_t size_bitarray = (maxsize + 1) / sizeof(uint64_t);
+    size_t size_bitarray = (maxsize + 1) / sizeof(unsigned long long);
     vs->size_bitarray = size_bitarray;
-    vs->bitarray = (uint64_t*) malloc(size_bitarray * sizeof(uint64_t));
+    vs->bitarray = (unsigned long long*) malloc(size_bitarray * sizeof(unsigned long long));
 
     // init sparse array 
     // allocate to much memory to fill up whole array with all possible
     // vertices (no dublicates)
     vs->sparseArray = (uint32_t*) malloc(maxsize * sizeof(uint32_t));
+    vs->sparseArray[0] = NULL;
 
     //calculate size, where dense variant is more efficient
-    vs->sizeCrit = size_bitarray * sizeof(uint64_t) / sizeof(32);
+    vs->sizeCrit = size_bitarray * sizeof(unsigned long long) / sizeof(32);
 
     // set default as sparse
     vs->isdense = false;
 };
 
-void Vertexset_Add(uint32_t vertex, Vertexset* vs);
+void Vertexset_Add(uint32_t vertex, Vertexset* vs){
+    if (vs->isdense) {
+        SET_VISITED_CUSTOM((vs->bitarray), vertex);
+    }
+    
+};
+
+bool Vertexset_Contains(uint32_t vertexx, Vertexset* vs);
 
 bool Vertexset_TransformToDense(Vertexset* vs);
 
 bool Vertexset_TransformToSparse(Vertexset* vs);
 
+void Vertexset_Allreduce(Vertexset* vs, int VERTEXSET_OP);
 
-void Vertexset_Free(Vertexset* vs);
+
+void Vertexset_Deinit(Vertexset* vs){
+    free(vs->bitarray);
+    free(vs->sparseArray);
+};
 
--- src/vertexSet.h
+++ src/vertexSet.h
@@ -4,6 +4,7 @@
 #include <stddef.h>
 #include <stdint.h>
 #include <stdbool.h>
+#include <bitmap_custom.h>
 
 typedef struct Vertexset{
     size_t size;
@@ -11,7 +12,7 @@ typedef struct Vertexset{
     size_t sizeCrit;
     bool isdense;
 
-    uint64_t *bitarray;
+    unsigned long long *bitarray;
     uint32_t size_bitarray;
 
     uint32_t *sparseArray;
@@ -21,13 +22,19 @@ void Vertexset_Init(Vertexset* vs, uint32_t maxsize);
 
 void Vertexset_Add(uint32_t vertex, Vertexset* vs);
 
+bool Vertexset_Contains(uint32_t vertexx, Vertexset* vs);
+
 bool Vertexset_TransformToDense(Vertexset* vs);
 
 bool Vertexset_TransformToSparse(Vertexset* vs);
 
+void Vertexset_Allreduce(Vertexset* vs, int VERTEXSET_OPERATION);
 
-void Vertexset_Free(Vertexset* vs);
+void Vertexset_Deinit(Vertexset* vs);
 
 
+#define VERTEXSET_OR 0
+#define VERTEXSET_AND 1
+#define VERTEXSET_XOR 2
 
 #endif // VERTEXSET
