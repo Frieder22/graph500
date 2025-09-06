1ba93732890ff4466fa33616cbf17b558565fa83
Frieder Wolpert-Veit <frieder@wolpert-veit.de>
2025-09-07 01:06:10 +0200

    first try adapting iterators to shifted bitarray

--- vertexSet/vertexSetIterator.c
+++ vertexSet/vertexSetIterator.c
@@ -13,7 +13,12 @@ void vertexSetIterator_Init(vertexSetIterator* const it, Vertexset* const vs) {
     if (it->isdense) {
         it->wordIdx = 0;
         it->wordShift = 0;
-        it->word = vs->bitArray[0];
+        if (vs->approx_halfing) {
+            it->word = vs->bitArray[vs->indexShift];
+        } else {
+            it->word = vs->bitArray[0];
+        }
+
     } else {
         // setting for sparse representation
         it->sparseIdx = 0;
@@ -22,30 +27,60 @@ void vertexSetIterator_Init(vertexSetIterator* const it, Vertexset* const vs) {
 
 bool vertexSetIterator_Has_next(vertexSetIterator* const it){
     if (it->isdense) {
-        for ( size_t i = it->wordIdx; i < it->vs->size_bitarray; i++){
-            // find a word thats not 0
-            if (it->vs->bitArray[i] != 0ULL){
-                // find the 1s in the word
-                for (int j = it->wordShift; j < ulong_bits; j++) {
-                    if (it->word &1UL == 1) { //found a 1
-                        // save current vertex
-                        it->currVertex = i * ulong_bits + j;
-                        
-                        //shift one to right
-                        it->word >>= 1; 
-                        // save progress
-                        it->wordIdx = i;
-                        it->wordShift = j + 1;
-                        return true;
+        if (!it->vs->approx_halfing){
+            for ( size_t i = it->wordIdx; i < it->vs->size_bitarray; i++){
+                // find a word thats not 0
+                if (it->vs->bitArray[i] != 0ULL){
+                    // find the 1s in the word
+                    for (int j = it->wordShift; j < ulong_bits; j++) {
+                        if (it->word &1UL == 1) { //found a 1
+                            // save current vertex
+                            it->currVertex = i * ulong_bits + j;
+                            
+                            //shift one to right
+                            it->word >>= 1; 
+                            // save progress
+                            it->wordIdx = i;
+                            it->wordShift = j + 1;
+                            return true;
+                        }
+                        it->word >>= 1; //shift one to right
                     }
-                    it->word >>= 1; //shift one to right
                 }
+                it->wordIdx = i + 1;
+                it->wordShift = 0;
+                it->word = it->vs->bitArray[i+1];
             }
-            it->wordIdx = i + 1;
-            it->wordShift = 0;
-            it->word = it->vs->bitArray[i+1];
+            return false;
+        } else {
+            for ( size_t wordIdx = it->wordIdx; wordIdx < it->vs->size_bitarray; wordIdx++){
+                int i = (wordIdx - it->vs->indexShift + it->vs->size_bitarray) % it->vs->size_bitarray;
+                // find a word thats not 0
+                if (it->vs->bitArray[i] != 0ULL){
+                    // find the 1s in the word
+                    for (int j = it->wordShift; j < ulong_bits; j++) {
+                        if (it->word &1UL == 1) { //found a 1
+                            // save current vertex
+                            it->currVertex = i * ulong_bits + j;
+                            
+                            //shift one to right
+                            it->word >>= 1; 
+                            // save progress
+                            it->wordIdx = wordIdx;
+                            it->wordShift = j + 1;
+                            return true;
+                        }
+                        it->word >>= 1; //shift one to right
+                    }
+                }
+                it->wordIdx = wordIdx + 1;
+                it->wordShift = 0;
+                it->word = it->vs->bitArray[i+1];
+            }
+            return false;
         }
-        return false;
+        
+        
     } else {
         return it->sparseIdx < it->vs->sizeSparse;
     }
